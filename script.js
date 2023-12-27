87a9bbe48f6613315edab14361fd7718362c3233
yash Niwane <yashniwane.openinfotech@gmail.com>
2023-12-27 22:23:22 +0530

    updates

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('noButton');
-    const buttonWidth = 70; // Adjusted to the desired width
-    const buttonHeight = 50; // Adjusted to the desired height
-  
-    button.addEventListener('click', () => alert('You clicked me!'));
-  
-    button.addEventListener('mouseover', () => {
-      const maxWidth = window.innerWidth - buttonWidth;
-      const maxHeight = window.innerHeight - buttonHeight;
-  
-      const randomX = Math.floor(Math.random() * maxWidth);
-      const randomY = Math.floor(Math.random() * (maxHeight / 2)) * -1; // Move upward
-  
-      button.style.position = 'absolute';
-      button.style.transform = `translate(${randomX}px, ${randomY}px)`;
-    });
-  });
+  const button = document.getElementById('noButton');
+  const buttonWidth = 70; // Adjusted to the desired width
+  const buttonHeight = 50; // Adjusted to the desired height
+
+  // Function to move the button to random coordinates
+  const moveButton = () => {
+    const maxWidth = window.innerWidth - buttonWidth;
+    const maxHeight = window.innerHeight - buttonHeight;
+
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * (maxHeight / 2)) * -1; // Move upward
+
+    button.style.position = 'absolute';
+    button.style.transform = `translate(${randomX}px, ${randomY}px)`;
+  };
+
+  // Event listener for mouseover
+  button.addEventListener('mouseover', moveButton);
+
+  // Event listener for click
+  button.addEventListener('click', moveButton);
+});
