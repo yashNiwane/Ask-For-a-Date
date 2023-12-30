dd3a4fad0cc2955b536ec6a2316d6d3c806b0c59
yash Niwane <yashniwane.openinfotech@gmail.com>
2023-12-30 13:02:19 +0530

    updates

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+setTimeout(function() {
+  document.getElementById("myAudio").play();
+}, 1000); 
+
+   
+      const audio = document.getElementById('myAudio');
+      audio.volume = 0.2;
+
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('noButton');
-  const buttonWidth = 70; // Adjusted to the desired width
-  const buttonHeight = 50; // Adjusted to the desired height
+  const buttonWidth = 70; 
+  const buttonHeight = 50; 
 
-  // Function to move the button to random coordinates
+  
   const moveButton = () => {
     const maxWidth = window.innerWidth - buttonWidth;
     const maxHeight = window.innerHeight - buttonHeight;
 
-    const randomX = Math.floor(Math.random() * maxWidth);
-    const randomY = Math.floor(Math.random() * (maxHeight / 2)) * -1; // Move upward
+    const randomX = Math.floor(Math.random() * maxWidth/2) * -1;
+    const randomY = Math.floor(Math.random() * (maxHeight / 2)) * -1; 
 
     button.style.position = 'absolute';
+    button.style.transition = 'transform 0.3s'; 
     button.style.transform = `translate(${randomX}px, ${randomY}px)`;
   };
 
-  // Event listener for mouseover
-  button.addEventListener('mouseover', moveButton);
 
-  // Event listener for click
+
+
+
+
+
+  
+  button.addEventListener('mouseover', moveButton);
   button.addEventListener('click', moveButton);
 });
+
